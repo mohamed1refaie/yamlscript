948068b51ceccfa54694d9cbc59c0677a4fec484
Mohamed Refaie <mohamed1refaie@hotmail.com>
2020-03-25 23:23:04 +0200

    printing the the commands

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const YAML = require('yaml');
 const fs = require("fs");
 
-const file = fs.readFileSync('./file.yml', 'utf8')
+const file = fs.readFileSync('./file2.yml', 'utf8')
 let res = YAML.parse(file);
-console.log(res);
+//console.log(res);
+const read =  (arr,indent) => {
+    for(let i=0;i<arr.length;i++){
+        let str=indent;
+        str+="+- ";
+        str+=arr[i].c;
+        console.log(str);
+        if(arr[i].next)
+            read(arr[i].next,i!=arr.length-1?indent+"|  ":indent+"   ");
+    }
+}
+read(res,"");
