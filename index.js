512d7d914a7e7b985dbef8d88b5b0d7c9d72275b
Mohamed Refaie <mohamed1refaie@hotmail.com>
2020-03-27 21:19:36 +0200

    creating logs and making the names of the commands more unique

--- index.js
+++ index.js
@@ -27,46 +27,89 @@ const snakeCase = str => {
     return res;
   };
 //console.log(res);
-function read (arr,indent) {
+
+let logName;
+const writeLog = log => {
+  fs.appendFile("_logs/" + logName, log + "\n", function(err) {
+    if (err) throw err;
+  });
+};
+const createLog = () => {
+  fs.mkdir("_logs", { recursive: true }, err => {
+    if (err) throw err;
+  });
+  let dateTime = new Date();
+  dateTime = dateTime.toISOString();
+  dateTime = dateTime.replace(/-/gi, "_");
+  dateTime = dateTime.replace(/:/gi, "_");
+  dateTime = dateTime.replace(".", "_");
+  dateTime = dateTime + "-debug.log";
+  logName = dateTime;
+  fs.writeFile("./_logs/" + dateTime, dateTime + "\n", { flag: "w" }, err => {
+    // In case of a error throw err.
+    if (err) {
+      throw err;
+    }
+  });
+};
+
+
+function read (arr,indent,level) {
+  
     for(let i=0;i<arr.length;i++){
         let str=indent;
-        str+="+- ";
+        if(i==arr.length-1)str+='└── '
+        else str+="├── ";
         str+=arr[i].c;
-        spinnies.add(arr[i].c, {
+        spinnies.add(arr[i].c+""+level+""+i, {
             text: str,
             color: "gray"
           });
         //spinnies.update(snakeCase(arr[i].c),{status:"non-spinnable"});
         //console.log(str);
         if(arr[i].next)
-            read(arr[i].next,i!=arr.length-1?indent+"|  ":indent+"   ");
+            read(arr[i].next,i!=arr.length-1?indent+"|  ":indent+"   ",level+1);
         //spinnies.succeed(str);
     }
 }
-read(res,"");
-const exec = async (command) => {
-    spinnies.update(command.c,{status:"spinning", color:"yellow"});
+createLog();
+read(res,"",0);
+const stopChilds = async (arr,level) =>{
+  for(let i=0;i<arr.length;i++){
+    spinnies.update(arr[i].c+""+level+""+i,{status:"fail", failColor:"gray"});
+    writeLog("command : "+`'`+arr[i].c+`',`+" terminated");
+    if(arr[i].next)
+      stopChilds(arr[i].next,level+1);
+  }
+}
+const exec = async (command,level,i) => {
+    spinnies.update(command.c+""+level+""+i,{status:"spinning", color:"yellow"});
         try {
             const { stdout, stderr } = await promiseExec(command.c);
-            if (stderr) console.log(stderr);
-            if (stdout) console.log(stdout);
-            spinnies.succeed(command.c);
+            if (stderr) writeLog(stderr);
+            if (stdout) writeLog(stdout);
+            spinnies.succeed(command.c+""+level+""+i);
+            writeLog("command : "+`'`+command.c+`',`+" succeed");
+            if(command.next)
+              execCommands(command.next,level+1);
             //return { stdout, stderr };
           } catch (e) {
-            spinnies.fail(command.c);
-            console.log(e.stderr);
+            spinnies.fail(command.c+""+level+""+i);
+            writeLog("command : "+`'`+command.c+`',`+" failed");
+            writeLog(e.stderr);
+            if(command.next)
+              stopChilds(command.next,level+1)
           }
-        if(command.next)
-          execCommands(command.next);
+        
 }
 //spinnies.update(snakeCase(res[0].c),{status:"spinning", color:"yellow"});
-const execCommands = async (arr) => {
+const execCommands = async (arr,level) => {
      for(let i=0;i<arr.length;i++) {
-        exec(arr[i]);
+        exec(arr[i],level,i);
         
      }
 }
 
 
 
-execCommands(res);
+execCommands(res,0);
